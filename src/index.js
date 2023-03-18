78f9f531c6c81e7f78f343b848f44ccc735e4309
Inna Zorina <innazorina0101@gmail.com>
2023-03-18 13:44:02 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import countryInfoTpl from './templates/country-info.hbs';
+import countryListTpl from './templates/country-list.hbs';
+import API from "./fetchCountries.js";
 
 const DEBOUNCE_DELAY = 300;
 
@@ -16,9 +19,25 @@ const refs = {
 refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY))
 
 function onInput(e) {
-    e.preventDefault();
+    clearCountryList();
+    const countryName = e.target.value.trim();
 
-    fetchCountries()
-    .then(renderCountryList)
-    .catch(error => console.log(error))
+    if (countryName === '') {
+        return;
+    }
+
+API.fetchCountries(cantryName)
+.then(cantryCards => {
+    const numberOfCountries = cantryCards.length;
+
+    if (numberOfCountries > 10) Notify.info("Too many matches found. Please enter a more specific name.");
+
+    else if (numberOfCountries >= 2 && numberOfCountries <= 10) {
+        renderCountryList(countryCards);
+    } 
+    else if (numberOfCountries === 1) {
+        renderCountryInfo(countryCards);
+    }
+})
+.catch(onFetchError)
 }
